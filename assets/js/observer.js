3b787a81987c587212e75750bbee42f17d77ee38
enosiii <121915517+enosiii@users.noreply.github.com>
2024-12-02 16:26:49 +0800

    Update observer.js

--- assets/js/observer.js
+++ assets/js/observer.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const sections = document.querySelectorAll("section"); // Select all sections
   const options = {
     root: null, // Use viewport as the root
-    threshold: 0.1, // Trigger when 60% of the section is visible
+    threshold: 0, // Trigger when 60% of the section is visible
     rootMargin: "-50% 0px -50% 0px", // Adjust to trigger when the section's center is near the viewport's center
   };
 
